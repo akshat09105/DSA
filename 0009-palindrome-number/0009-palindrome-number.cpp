443910f529a56ff382df7bd123c9fb7ef882b620
Akshat Gupta <akshat9105@gmail.com>
2025-08-14 17:26:27 +0530

    Time: 0 ms (100%), Space: 8.5 MB (67.6%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        //finding total no of digits
+        if(x>0){
+        int s=x;int n=x;int digits=0;long int sum=0;
+        while(n!=0){
+            n=n/10;
+            digits=digits+1;
+        }
+        int d=digits;
+        for(int i=digits;i>0;i--){
+            long int sum1=1;
+            int digit=s%10;
+            for(int i=d-1;i>0;i--){
+                sum1=10*sum1;
+            }
+            d--;
+            sum=sum+digit*sum1;
+            s=s/10;
+            
+        }
+        if(x==sum){
+            return true;
+        }
+        else{
+            return false;
+        }
+        }
+        
+        else if(x==0){
+            return true;
+        }
+        
+        
+        else{
+            return false;
+        }
+    }
+    
+    
+};
