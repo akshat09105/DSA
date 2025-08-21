ecd0e54b078cea49d615b1becd2581adfa34a121
Akshat Gupta <akshat9105@gmail.com>
2025-08-21 23:22:24 +0530

    Time: 0 ms (100%), Space: 9 MB (7.24%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long double y=1;
+        
+        y=(long double)pow(x,n);
+        
+        
+    return y;    
+    }
+    
+};
