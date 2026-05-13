476c128a12a39a986ed472b6f4a15f48a0c0b96d
Akshat Gupta <akshat9105@gmail.com>
2026-05-13 18:49:41 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (41.58%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -5,24 +5,17 @@ public:
         int right=n-1;
         int left=0;
         int mid=0;
+        int ans=nums.size();
         while(left<=right){
             mid=(left+right)/2;
             if(nums[mid]<target){
                 left=mid+1;
             }
-            else if(nums[mid]==target){
-                return mid;
-            }
-            else{
+            else if(nums[mid]>=target){
+                ans=mid;
                 right=mid-1;
             }
         }
-        if(nums[mid]>target){
-            return mid;
-        }
-        else{
-            return mid+1;
-        }
-
+    return ans;
     }
 };
