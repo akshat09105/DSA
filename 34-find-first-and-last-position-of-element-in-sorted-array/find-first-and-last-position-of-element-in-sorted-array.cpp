d4d516b021a080a6413d2a53a3cfd85dcbb8de14
Akshat Gupta <akshat9105@gmail.com>
2026-05-14 11:35:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (18.13%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,35 +4,37 @@ public:
     vector<int> result = {-1, -1};
     
     // 1. First Position dhoondne ke liye
-    int left = 0, right = nums.size() - 1;
+    int left = 0, right = nums.size() - 1;int mid;
     while (left <= right) {
         int mid = (left + right) / 2; // Overflow se bachne ke liye better way
-        if (nums[mid] == target) {
-            result[0] = mid;      // Potential answer mil gaya
-            right = mid - 1;      // Par left mein check karte raho "extreme left" ke liye
-        } else if (nums[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+        mid=(left+right)/2;
+        if(nums[mid]>target){
+            right=mid-1;
+        }
+        else if(nums[mid]==target){
+            right=mid-1;
+            result[0]=mid;
+        }
+        else{
+            left=mid+1;
         }
     }
-
-    // 2. Last Position dhoondne ke liye (Sirf tab dhoondo agar pehla mil gaya ho)
-    if (result[0] != -1) {
-        left = 0, right = nums.size() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                result[1] = mid;  // Potential answer mil gaya
-                left = mid + 1;   // Par right mein check karte raho "extreme right" ke liye
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+    //second position
+    left = 0;right = nums.size() - 1;
+    while (left <= right) {
+        int mid = (left + right) / 2; // Overflow se bachne ke liye better way
+        mid=(left+right)/2;
+        if(nums[mid]>target){
+            right=mid-1;
+        }
+        else if(nums[mid]==target){
+            left=mid+1;
+            result[1]=mid;
+        }
+        else{
+            left=mid+1;
         }
     }
-
     return result;
 }
 };
