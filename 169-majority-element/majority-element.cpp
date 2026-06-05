ed9f03193563ce4adf4a95b401d2044495530ca5
Akshat Gupta <akshat9105@gmail.com>
2026-06-05 10:12:45 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (99.10%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -2,19 +2,30 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         //applying mools voting algo
-        int el;int count=0;
+        int cnt=0;int el;
         for(int i=0;i<nums.size();i++){
-            if(count==0){
+            if(cnt==0){
                 el=nums[i];
             }
             if(el==nums[i]){
-                count++;
+                cnt++;
             }
             else{
-                count--;
+                cnt--;
             }
-
         }
-        return el;
-    }
+        return el; 
+        /*checking if there a majority element exist of not....if majority element exist el will represent that only
+        cnt=0;
+        for(int i=0;i<nums.size();i++){
+            if(el==nums[i]){
+                cnt++;
+            }
+        }
+        if(cnt>(nums.size()/2)){
+            return el;
+        }
+        return -1;
+        */
+   }
 };
