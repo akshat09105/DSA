e8513762294445ec9c1fa1823e4be9d3186b4b8e
Akshat Gupta <akshat9105@gmail.com>
2026-06-18 18:00:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (60.12%) - LeetSync

---
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+      /*there is BS on answer can be applied in this question as no min or max needs to
+      found*/
+      /*but BS search needs can be applied as we need to search in sorted in array so bs clicked*/
+      int left=0;int right=arr.size()-1;
+      while(left<=right){
+        int mid=(left+right)/2;
+        int missing=arr[mid]-(mid+1);
+        if(missing<k){
+            left=mid+1;
+        }
+        else{
+            right=mid-1;
+        }
+      }
+      return k+(right+1);
+
+
+    }
+};
