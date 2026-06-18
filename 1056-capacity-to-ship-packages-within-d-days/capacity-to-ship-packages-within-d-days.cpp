766519bc2ba663490c06a49751d5777c7b19f6b2
Akshat Gupta <akshat9105@gmail.com>
2026-06-18 11:16:31 +0530

    Time: 12 ms (51.61%) | Memory: 35 MB (52.97%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     int ship_capable(vector<int>&weights,int cap){
-        int days=1;int load=0;
-        for(int i=0;i<weights.size();i++){
-            if(load+weights[i]>cap){
-                days++;
-                load=weights[i];
-            }
-            else{
-                load+=weights[i];
-            }
-        }
-        
-        return days;
+       int load=0;int days=1;
+       for(int i=0;i<weights.size();i++){
+           if(load+weights[i]>cap){
+            days++;
+            load=weights[i];
+           }
+           else{
+            load+=weights[i];
+           }
+       }
+       return days;
+
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        long long sum=0;int maxi=INT_MIN;
+        int maxi=INT_MIN;int sum=0;
         for(int i=0;i<weights.size();i++){
-            sum+=weights[i];
             maxi=max(maxi,weights[i]);
+            sum+=weights[i];
         }
-        int left=maxi;int right=sum;int ans;
+        int left=maxi;int right=sum;int mid;int ans=-1;
         while(left<=right){
-            int mid=(left+right)/2;
+            mid=(left+right)/2;
             if(ship_capable(weights,mid)<=days){
+                right=mid-1;
                 ans=mid;
-                right=mid-1;//decreasing capacity there by increasing days
             }
             else{
                 left=mid+1;
