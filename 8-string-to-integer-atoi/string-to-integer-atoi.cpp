af5b10c05f900cee9efc01ab68735d17b24f62c9
Akshat Gupta <akshat9105@gmail.com>
2026-06-28 08:20:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (7.46%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int pos_check(string &s,int &n){
+        int x;
+        if(s[0]=='-'){
+            x=-1;
+            s=s.substr(1,n);
+            n--;
+        }
+        else if(s[0]=='+'){
+            x=1;
+            s=s.substr(1,n);
+            n--;
+        }
+        else{
+            x=1;
+        }
+        return x;
+    }
+    int myAtoi(string s) {
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]!=' '){
+                s=s.substr(i,n);
+                break;
+            }
+            
+        }
+        n=s.size();
+        long long ans=0;
+        int pos=pos_check(s,n);//if pos is 1 then number is positive or -1 if number i -ve
+        for(int i=0;i<n;i++){
+            if((s[i]-'0')>=0&&(s[i]-'0')<=9){
+                ans=ans*10+(s[i]-'0');
+                if(ans>INT_MAX){
+                    if(pos==-1){
+                        return INT_MIN;
+                    }
+                    else{
+                        return INT_MAX;
+                    }
+                }
+            }
+            else{
+                break;
+            }
+        }
+        ans=ans*pos;
+        if(ans<INT_MIN){
+            return INT_MIN;
+        }
+       if(ans>INT_MAX){
+           return INT_MAX;
+       }
+       return ans;
+
+        
+    }
+};
