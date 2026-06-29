cb51bcb01bc89849064e93c143f4ecf9368bfc1e
Akshat Gupta <akshat9105@gmail.com>
2026-06-29 12:47:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (28.41%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char>mpp;
+        unordered_map<char,char>mpp1;
+        unordered_map<char,char>mpp2;
         int n=s.size();
         for(int i=0;i<n;i++){
-            auto it=mpp.find(s[i]);
-            if(it==mpp.end()){
-                for(auto j:mpp){
-                    if(j.second==t[i]){
-                        return false;
-                    }
-                }
-                mpp[s[i]]=t[i];
+            auto it1=mpp1.find(s[i]);
+            auto it2=mpp2.find(t[i]);
+            if(it1==mpp1.end()&& it2==mpp2.end()){
+                mpp1[s[i]]=t[i];
+                mpp2[t[i]]=s[i];
                 
             }
+            else if(it1==mpp1.end() && it2!=mpp1.end()){
+                return false;
+            }
+            else if(it1!=mpp1.end() && it2==mpp1.end()){
+                return false;
+            }
             else{
-                if(it->second!=t[i]){
+                if(it1->second!=t[i]||it2->second!=s[i]){
                     return false;
                 }
 
