64d6db392066b3cbef8b7d69fbca98a771b798bb
Akshat Gupta <akshat9105@gmail.com>
2026-06-29 15:42:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (28.41%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
+        int n=s.size();
         unordered_map<char,char>mpp1;
         unordered_map<char,char>mpp2;
-        int n=s.size();
         for(int i=0;i<n;i++){
             auto it1=mpp1.find(s[i]);
             auto it2=mpp2.find(t[i]);
-            if(it1==mpp1.end()&& it2==mpp2.end()){
-                mpp1[s[i]]=t[i];
-                mpp2[t[i]]=s[i];
-                
+            if(it1==mpp1.end() && it2==mpp2.end()){
+                mpp1[s[i]]=t[i];//a->b
+                mpp2[t[i]]=s[i];//b->a
+            
             }
-            else if(it1==mpp1.end() && it2!=mpp2.end()){
-                return false;
+            else if(it1==mpp1.end() && it2!=mpp2.end()){//c->b will be invalid
+                return false;  //as b already has one to one relation with therefore present in mpp2
             }
             else if(it1!=mpp1.end() && it2==mpp2.end()){
                 return false;
             }
             else{
-                if(it1->second!=t[i]||it2->second!=s[i]){
+                if(it1->second==t[i] && it2->second==s[i]){
+                    continue;
+                }
+                else{
                     return false;
                 }
-
             }
         }
         return true;
