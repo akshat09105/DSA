2403e441df6245e453d9be0dfacebbbc42c5873e
Akshat Gupta <akshat9105@gmail.com>
2026-06-30 22:57:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (25.72%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,15 +4,15 @@ public:
         if(s.size()!=t.size()){
             return false;
         }
-        unordered_map<char,int>mpp;
+        vector<int>mpp(26,0);
         for(int i=0;i<s.size();i++){
-            mpp[s[i]]++;
+            mpp[s[i]-'a']++;
         }
         for(int i=0;i<t.size();i++){
-            mpp[t[i]]--;
+            mpp[t[i]-'a']--;
         }
-        for(auto it:mpp){
-            if(it.second!=0){
+        for(int i=0;i<26;i++){
+            if(mpp[i]!=0){
                 return false;
             }
         }
