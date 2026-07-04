58738815d4797425a2e61abc65433e71fb9a4e52
Akshat Gupta <akshat9105@gmail.com>
2026-07-04 12:30:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (95.74%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -17,16 +17,12 @@ public:
         if(nums[mid]>nums[mid+1]&& nums[mid]>nums[mid-1]){
             return  mid;
         }
-        else if(nums[mid]>nums[mid-1]){//peak is on right
-            low=mid+1;
-        }
-        else if(nums[mid]>nums[mid+1]){//peak is on left
+        else if(nums[mid]<nums[mid-1]){
             high=mid-1;
         }
         else{
-            high=mid-1;
+            low=mid+1;
         }
-
        }
        return -1;
 
