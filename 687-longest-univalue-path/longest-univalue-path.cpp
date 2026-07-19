8ddccfdd8652fcfb43046ab0da0a52e98505f6ee
Akshat Gupta <akshat9105@gmail.com>
2026-07-20 01:00:14 +0530

    Time: 0 ms (100.00%) | Memory: 72.4 MB (53.90%) - LeetSync

--- 687-longest-univalue-path/longest-univalue-path.cpp
+++ 687-longest-univalue-path/longest-univalue-path.cpp
@@ -6,37 +6,43 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
  * };
  */
 
 class Solution {
 public:
-    int ans = 0;
 
-    int dfs(TreeNode* root) {
-        if (root == nullptr)
+    int solve(TreeNode* root, int &maxi) {
+
+        if(root == nullptr)
             return 0;
 
-        int left = dfs(root->left);
-        int right = dfs(root->right);
+        int lh = solve(root->left, maxi);
+        int rh = solve(root->right, maxi);
 
         int leftPath = 0;
         int rightPath = 0;
 
-        if (root->left && root->left->val == root->val)
-            leftPath = left + 1;
+        if(root->left && root->left->val == root->val)
+            leftPath = lh + 1;
 
-        if (root->right && root->right->val == root->val)
-            rightPath = right + 1;
+        if(root->right && root->right->val == root->val)
+            rightPath = rh + 1;
 
-        ans = max(ans, leftPath + rightPath);
+        // Same as diameter
+        maxi = max(maxi, leftPath + rightPath);
 
+        // Return longest chain to parent
         return max(leftPath, rightPath);
     }
 
     int longestUnivaluePath(TreeNode* root) {
-        dfs(root);
-        return ans;
+
+        int maxi = 0;
+        solve(root, maxi);
+
+        return maxi;
     }
 };
