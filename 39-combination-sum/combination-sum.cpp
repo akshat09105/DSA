d19ff074d2c4466278e3308efcb6aa06f6335a8e
Akshat Gupta <akshat9105@gmail.com>
2026-07-24 09:07:52 +0530

    Time: 1012 ms (5.06%) | Memory: 382.4 MB (5.84%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> Combinations(vector<int>candidates,vector<vector<int>>ans,vector<int>ds,int target,int index,int n){
-        
-        if(index==n){
+        if(index==n||target<=0){
             if(target==0){
                 ans.push_back(ds);
             }
