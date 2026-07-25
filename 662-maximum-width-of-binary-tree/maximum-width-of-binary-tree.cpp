8544147294bf0dc0279b7930e734a65f881789f1
Akshat Gupta <akshat9105@gmail.com>
2026-07-25 17:54:21 +0530

    Time: 2 ms (16.66%) | Memory: 19.8 MB (45.37%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -6,30 +6,44 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        queue<pair<TreeNode*,long long>>q;
-        q.push({root,0});long long ans=0;
-        while(!q.empty()){
-        long long left,right;
-        int size=q.size();
-        long long minE=q.front().second;
-        for(int i=0;i<size;i++){
-            long long curr=q.front().second-minE;
-            TreeNode* node=q.front().first;
-            q.pop();
-            if(i==0)left=curr;
-            if(i==size-1)right=curr;
-            if(node->left)q.push({node->left,curr*2+1});
-            if(node->right)q.push({node->right,curr*2+2});
+        if (root == nullptr) return 0;
+
+        queue<pair<TreeNode*, long long>> q;
+        q.push({root, 0});
+
+        long long ans = 1;
+
+        while (!q.empty()) {
+            int size = q.size();
+            long long minIndex = q.front().second;
+            long long first, last;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front().first;
+                long long curr = q.front().second - minIndex;
+                q.pop();
+
+                if (i == 0) first = curr;
+                if (i == size - 1) last = curr;
+
+                if (node->left)
+                    q.push({node->left, 2 * curr + 1});
+
+                if (node->right)
+                    q.push({node->right, 2 * curr + 2});
+            }
+
+            ans = max(ans, last - first + 1);
         }
-        ans=max(ans,right-left+1);
-        }
-        return ans;
-        
+
+        return (int)ans;
     }
 };
