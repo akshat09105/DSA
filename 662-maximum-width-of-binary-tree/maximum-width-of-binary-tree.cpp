b033bf82ea10bfee0fc1331ff6adcc6e5cd9cf8b
Akshat Gupta <akshat9105@gmail.com>
2026-07-28 14:08:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (95.95%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -14,36 +14,25 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if (root == nullptr) return 0;
-
-        queue<pair<TreeNode*, long long>> q;
-        q.push({root, 0});
-
-        long long ans = 1;
-
-        while (!q.empty()) {
-            int size = q.size();
-            long long minIndex = q.front().second;
-            long long first, last;
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front().first;
-                long long curr = q.front().second - minIndex;
-                q.pop();
-
-                if (i == 0) first = curr;
-                if (i == size - 1) last = curr;
-
-                if (node->left)
-                    q.push({node->left, 2 * curr + 1});
-
-                if (node->right)
-                    q.push({node->right, 2 * curr + 2});
-            }
-
-            ans = max(ans, last - first + 1);
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
+        long long ans=INT_MIN;
+        while(!q.empty()){/*we will do level order traversal on top and bottom view also we did level order traversal only but there we don't need to level it up*/ 
+        //but here we will need to do it levelly as we need to find first and last node of each level
+        int mini=q.front().second;
+        long long size=q.size();
+        long long first,last;
+        for(int i=0;i<size;i++){
+            TreeNode* node=q.front().first;
+            long long curr=q.front().second-mini;
+            q.pop();
+            if(i==0)first=curr;
+            if(i==size-1)last=curr;
+            if(node->left)q.push({node->left,curr*2+1});
+            if(node->right)q.push({node->right,curr*2+2});
+        }        
+        ans=max(ans,last-first+1);
         }
-
-        return (int)ans;
+        return ans;
     }
 };
