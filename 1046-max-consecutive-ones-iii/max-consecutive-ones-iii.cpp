53dc3bf7e8990d3e768bca139bfabc3b88fc2405
Akshat Gupta <akshat9105@gmail.com>
2026-08-11 10:08:27 +0530

    Time: 3 ms (32.81%) | Memory: 69.7 MB (34.94%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        /*will be using slinding window....maximum consecutive subarray with zero<=k
+         and after seeing subarray sliding window should be clicked in the mind*/
+        int left=0;int right=0;int maxlen=0;int zeroes=0;
+        while(right<nums.size()){
+            if(nums[right]==0)zeroes++;
+            while(zeroes>k && left<=right){
+                if(nums[left]==0)zeroes--;
+                left++;
+            }
+            if(zeroes<=k){
+                maxlen=max(maxlen,right-left+1);
+            }
+            right++;
+        }
+        return maxlen;
+    }
+};
