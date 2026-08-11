9938d8449ae768906df053a3184bd08a88cb23d5
Akshat Gupta <akshat9105@gmail.com>
2026-08-11 10:47:39 +0530

    Time: 0 ms (100.00%) | Memory: 69.6 MB (66.70%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -6,13 +6,13 @@ public:
         int left=0;int right=0;int maxlen=0;int zeroes=0;
         while(right<nums.size()){
             if(nums[right]==0)zeroes++;
-            while(zeroes>k){
+            if(zeroes>k){
                 if(nums[left]==0)zeroes--;
                 left++;
             }
-            
+            if(zeroes<=k){
                 maxlen=max(maxlen,right-left+1);
-            
+            }
             right++;
         }
         return maxlen;
