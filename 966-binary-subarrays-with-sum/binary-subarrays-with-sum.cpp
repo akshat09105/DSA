e1c14f33a6e0d6118f72dd6cef047969fe16948a
Akshat Gupta <akshat9105@gmail.com>
2026-08-14 13:16:07 +0530

    Time: 46 ms (5.35%) | Memory: 39.2 MB (12.29%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    int atMost(vector<int>& nums, int k) {
-        if (k < 0) return 0;
-
-        int l = 0;
-        int sum = 0;
-        int ans = 0;
-
-        for (int r = 0; r < nums.size(); r++) {
-            sum += nums[r];
-
-            while (sum > k) {
-                sum -= nums[l];
-                l++;
-            }
-
-            // All subarrays ending at r
-            // starting from l to r are valid
-            ans += (r - l + 1);
-        }
-
-        return ans;
-    }
-
     int numSubarraysWithSum(vector<int>& nums, int k) {
-        return atMost(nums, k) - atMost(nums, k - 1);
+        map<int,int>mpp;mpp[0]=1;int i=0;int sum=0;int ans=0;
+        while(i<nums.size()){
+            sum+=nums[i];
+            if(mpp.find(sum-k)!=mpp.end()){
+                ans+=mpp[sum-k];
+            }
+            mpp[sum]++;
+            i++;
+        }
+        return ans;
     }
 };
