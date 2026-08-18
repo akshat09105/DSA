9da0c8e47800459cb3fc64dad52bbe2e1e5afcab
Akshat Gupta <akshat9105@gmail.com>
2026-08-18 20:21:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (99.89%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void fn(vector<vector<int>>&ans,vector<int>&ds,int k,int n,int i){
+        if(ds.size()==k){
+            if(n==0){
+                ans.push_back(ds);
+            }
+            return;
+        }
+        for(;i<=9;i++){
+            if(i<=n){
+                ds.push_back(i);
+                fn(ans,ds,k,n-i,i+1);
+                ds.pop_back();
+            }
+            else{
+                break;
+            }
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        fn(ans,ds,k,n,1);
+        return ans;
+    }
+};
