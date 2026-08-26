75daeb0328d0babc20d8544ac83899a3255fa9b2
Akshat Gupta <akshat9105@gmail.com>
2026-08-26 14:16:51 +0530

    Create  Ninja’s Trainin_tabulation.cpp

---
+++ Ninja’s Trainin_tabulation.cpp
@@ -0,0 +1,24 @@
+//Tabulation
+
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    // Write your code here.
+    vector<vector<int>>dp(n,vector<int>(4,0));
+    dp[0][0]=max(points[0][1],points[0][2]);
+    dp[0][1]=max(points[0][0],points[0][2]);
+    dp[0][2]=max(points[0][0],points[0][1]);
+    dp[0][3]=max(points[0][0],max(points[0][1],points[0][2]));
+    for(int day=1;day<n;day++){
+        for(int last=0;last<4;last++){
+            int maxi=0;
+            for(int i=0;i<=2;i++){
+                if(i!=last){
+                    int point=points[day][i]+dp[day-1][i];
+                    maxi=max(maxi,point);
+                }
+            }
+            dp[day][last]=maxi;
+        }
+    }
+    return dp[n-1][3];
+}
