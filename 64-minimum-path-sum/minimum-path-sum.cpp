9987740d6eb0f6f70d4c94164d90526df7bf2c4f
Akshat Gupta <akshat9105@gmail.com>
2026-08-27 21:39:09 +0530

    Time: 4 ms (15.27%) | Memory: 16.4 MB (42.70%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -7,7 +7,7 @@ public:
         if(i<0||j<0){
             return INT_MAX;
         }
-        if(dp[i][j]!=-1)return dp[i][j];
+        if(dp[i][j]!=0)return dp[i][j];
         int up=f(i-1,j,grid,dp);
         int left=f(i,j-1,grid,dp);
         return dp[i][j]=(int)min((long long)grid[i][j]+up,(long long)grid[i][j]+left);
@@ -15,7 +15,7 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<int>>dp(n,vector<int>(m,0));
         return f(n-1,m-1,grid,dp);
     }
 };
