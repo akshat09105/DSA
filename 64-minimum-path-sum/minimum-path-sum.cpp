7356c40a5ee0c190aeec3022824dfc6881df1729
Akshat Gupta <akshat9105@gmail.com>
2026-08-27 22:21:02 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (42.70%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -16,6 +16,22 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>>dp(n,vector<int>(m,0));
-        return f(n-1,m-1,grid,dp);
+        dp[0][0]=grid[0][0];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0&&j==0)dp[i][j]=grid[i][j];
+                else{
+                    int up=INT_MAX;int left=INT_MAX;
+                    if(i>0){
+                        up=dp[i-1][j];
+                    }
+                    if(j>0){
+                        left=dp[i][j-1];
+                    }
+                    dp[i][j]=(int)min((long long)up+grid[i][j],(long long)left+grid[i][j]);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
