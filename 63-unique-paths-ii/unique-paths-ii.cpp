743d33db2a17e4ab573d04585a6d4427ddc27d47
Akshat Gupta <akshat9105@gmail.com>
2026-08-27 20:34:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (55.38%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -2,40 +2,20 @@ class Solution {
 public:
     //now recurssion converting it to memoization
     int f(int i,int j,vector<vector<int>>& obstacleGrid,vector<vector<int>>&dp){
+        if(i>=0&&j>=0&&obstacleGrid[i][j]==1)return 0;
         if(i==0&&j==0)return 1;
         if(i<0||j<0){
             return 0;
         }
-        if(obstacleGrid[i][j]==1)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
         int up=f(i-1,j,obstacleGrid,dp);
         int left=f(i,j-1,obstacleGrid,dp);
         return dp[i][j]=up+left;
     }
-    //sppace_optimized
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n=obstacleGrid.size();
         int m=obstacleGrid[0].size();
-        if(obstacleGrid[0][0]==1)return 0;
-        vector<int>prev(m,0);
-        for(int i=0;i<n;i++){
-            vector<int>temp(m,0);
-            for(int j=0;j<m;j++){
-                if(i==0&&j==0){
-                    temp[j]=1;
-                    continue;
-                }
-                if(obstacleGrid[i][j]==1){
-                    temp[j]=0;
-                    continue;
-                }
-                int up=0;int left=0;
-                if(i>0)up=prev[j];
-                if(j>0)left=temp[j-1];
-                temp[j]=up+left;
-            }
-            prev=temp;
-        }
-        return prev[m-1];
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,obstacleGrid,dp);
     }
 };
