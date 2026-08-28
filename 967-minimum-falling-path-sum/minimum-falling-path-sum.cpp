82aef7ea1fb0e89924fcfe5da063ff086dc3e864
Akshat Gupta <akshat9105@gmail.com>
2026-08-28 22:13:56 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (68.03%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -5,6 +5,7 @@ public:
         if(i==n-1){
             return matrix[i][j];
         }
+        
         if(dp[i][j]!=1e7){
             return dp[i][j];
         }
@@ -13,11 +14,25 @@ public:
         int Downright=matrix[i][j]+f(i+1,j+1,n,matrix,dp);
         return dp[i][j]=min(Downleft,min(Down,Downright));
     }
+    
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();int mini=INT_MAX;
         vector<vector<int>>dp(n,vector<int>(n,1e7));
         for(int j=0;j<n;j++){
-            mini=min(mini,f(0,j,n,matrix,dp));
+            dp[n-1][j]=matrix[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int Downleft=INT_MAX; int Downright=INT_MAX;
+                if(j>0)Downleft=matrix[i][j]+dp[i+1][j-1];
+                int Down=matrix[i][j]+dp[i+1][j];
+                if(j<n-1)Downright=matrix[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(Downleft,min(Down,Downright));
+            }
+        }
+    
+        for(int j=0;j<n;j++){
+            mini=min(dp[0][j],mini);
         }
         return mini;
     }
