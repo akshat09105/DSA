fdda9f42c7f0328f37706d122768f80bfed5f464
Akshat Gupta <akshat9105@gmail.com>
2026-08-29 12:09:39 +0530

    Time: 80 ms (12.25%) | Memory: 20.9 MB (74.92%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -22,11 +22,44 @@ public:
         return dp[i][j1][j2]=maxi;
 
     }
-    
-    int cherryPickup(vector<vector<int>>& grid) {
+    int cherriPickup(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
         return f(0,n,0,m-1,m,grid,dp);
+    }
+    //Tabulation
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
+        for(int j1=0;j1<m;j1++){
+            for(int j2=0;j2<m;j2++){
+                if(j1==j2)dp[n-1][j1][j2]=grid[n-1][j1];
+                else dp[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2];
+            }
+        }//base case
+        int maxi=INT_MIN;
+        for(int i=n-2;i>=0;i--){
+            for(int j1=m-1;j1>=0;j1--){
+                for(int j2=m-1;j2>=0;j2--){
+                    int maxi=INT_MIN;
+                    for(int dj1=-1;dj1<=1;dj1++){
+                        for(int dj2=-1;dj2<=1;dj2++){
+                            if(j1+dj1>=0&&j1+dj1<=m-1&&j2+dj2>=0&&j2+dj2<=m-1){
+                            if(j1==j2){
+                            maxi=max(grid[i][j1]+dp[i+1][j1+dj1][j2+dj2],maxi); 
+                            }
+                            else{
+                                maxi=max(grid[i][j1]+grid[i][j2]+dp[i+1][j1+dj1][j2+dj2],maxi);
+                            }
+                            }
+                        }
+                    } 
+                    dp[i][j1][j2]=maxi; 
+                }
+            }
+        }
+        return dp[0][0][m-1];
     }
 };
