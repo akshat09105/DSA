a3b53debcb69a319eebd8d92145998e5cde7f264
Akshat Gupta <akshat9105@gmail.com>
2026-08-29 12:18:40 +0530

    Time: 79 ms (13.62%) | Memory: 21 MB (56.17%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -29,7 +29,7 @@ public:
         return f(0,n,0,m-1,m,grid,dp);
     }
     //Tabulation
-    int cherryPickup(vector<vector<int>>& grid) {
+    int cherriiPickup(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
@@ -61,5 +61,41 @@ public:
             }
         }
         return dp[0][0][m-1];
+    }
+    //space Optimal
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>prev(m,vector<int>(m,-1));
+        for(int j1=0;j1<m;j1++){
+            for(int j2=0;j2<m;j2++){
+                if(j1==j2)prev[j1][j2]=grid[n-1][j1];
+                else prev[j1][j2]=grid[n-1][j1]+grid[n-1][j2];
+            }
+        }//base case
+        int maxi=INT_MIN;
+        for(int i=n-2;i>=0;i--){
+            vector<vector<int>>temp(m,vector<int>(m,0));
+            for(int j1=m-1;j1>=0;j1--){
+                for(int j2=m-1;j2>=0;j2--){
+                    int maxi=INT_MIN;
+                    for(int dj1=-1;dj1<=1;dj1++){
+                        for(int dj2=-1;dj2<=1;dj2++){
+                            if(j1+dj1>=0&&j1+dj1<=m-1&&j2+dj2>=0&&j2+dj2<=m-1){
+                            if(j1==j2){
+                            maxi=max(grid[i][j1]+prev[j1+dj1][j2+dj2],maxi); 
+                            }
+                            else{
+                                maxi=max(grid[i][j1]+grid[i][j2]+prev[j1+dj1][j2+dj2],maxi);
+                            }
+                            }
+                        }
+                    } 
+                    temp[j1][j2]=maxi; 
+                }
+            }
+            prev=temp;
+        }
+        return prev[0][m-1];
     }
 };
