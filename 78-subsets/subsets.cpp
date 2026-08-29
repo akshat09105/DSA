048f7d9d0b0aaba7789bb5f9426d3cb9b557ea0a
Akshat Gupta <akshat9105@gmail.com>
2026-08-29 17:09:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (82.23%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -4,8 +4,10 @@ public:
         int n=nums.size();vector<vector<int>>ans;
         for(int i=0;i<pow(2,n);i++){
             vector<int>ls;
-            for(int j=0;j<n;j++){
-                if(i&(1<<j))ls.push_back(nums[j]);
+            for(int index=0;index<n;index++){
+                if(i&1<<index){
+                    ls.push_back(nums[index]);
+                }
             }
             ans.push_back(ls);
         }
