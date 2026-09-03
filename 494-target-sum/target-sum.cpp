399e1b4698066a93376ee4036fc8ba6ef27d25e5
Akshat Gupta <akshat9105@gmail.com>
2026-09-03 22:35:27 +0530

    Time: 11 ms (57.24%) | Memory: 28.7 MB (17.84%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,56 +1,31 @@
 class Solution {
 public:
-    // Memoization
-    int f(int index, vector<int>& nums, int target,
-          vector<vector<int>>& dp, int& offset) {
-
-        // Target outside possible range
-        if (target < -offset || target > offset)
-            return 0;
-
-        if (index == 0) {
-            if (target - nums[index] == 0 &&
-                target + nums[index] == 0) {
+    //Memoization
+    int f(int index,vector<int>&nums,int target,vector<vector<int>>&dp,int &offset){
+        
+        if(index==0){
+            if(target-nums[index]==0&&target+nums[index]==0){
                 return 2;
             }
-
-            if (target - nums[index] == 0 ||
-                target + nums[index] == 0) {
+            if(target-nums[index]==0||target+nums[index]==0){
                 return 1;
             }
-
             return 0;
         }
-
-        if (dp[index][target + offset] != -1)
-            return dp[index][target + offset];
-
-        int left = f(index - 1, nums,
-                     target - nums[index], dp, offset);
-
-        int right = f(index - 1, nums,
-                      target + nums[index], dp, offset);
-
-        return dp[index][target + offset] = left + right;
+        if(dp[index][target+offset]!=-1)return dp[index][target+offset];
+        int left=f(index-1,nums,target-nums[index],dp,offset);
+        
+        int right=f(index-1,nums,target+nums[index],dp,offset);
+        
+        return dp[index][target+offset]=left+right;
     }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-
-        int sum = 0;
-
-        for (int x : nums)
-            sum += x;
-
-        if (target < -sum || target > sum)
-            return 0;
-
-        int offset = sum;
-
-        vector<vector<int>> dp(
-            nums.size(),
-            vector<int>(2 * sum + 1, -1)
-        );
-
-        return f(nums.size() - 1, nums, target, dp, offset);
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        int offset=sum+abs(target);
+        vector<vector<int>>dp(nums.size(),vector<int>(2*offset+abs(target)+1,-1));
+        return f(nums.size()-1,nums,target,dp,offset);
     }
 };
