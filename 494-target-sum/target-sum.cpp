5c18dcd35e688bf828c490fdb540ea4a55ee0067
Akshat Gupta <akshat9105@gmail.com>
2026-09-09 01:17:08 +0530

    Time: 3 ms (86.11%) | Memory: 15.5 MB (46.21%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -12,6 +12,7 @@ public:
         if(arr[index]<=sum)take=f(index-1,sum-arr[index],arr,dp);
         return dp[index][sum]=take+not_take;
     }
+    //tabulation
     int findTargetSumWays(vector<int>& nums, int target) {
         int sum=0;
         for(int i=0;i<nums.size();i++){
@@ -21,7 +22,22 @@ public:
         if((sum-target)%2!=0)return 0;
         int s2=(sum-target)/2;
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(s2+1,-1));
-        return f(n-1,s2,nums,dp);
+        vector<vector<int>>dp(n,vector<int>(s2+1,0));
+        if(nums[0]==0)dp[0][0]=2;
+        else{
+            dp[0][0]=1;
+            if(nums[0]<=s2){
+            dp[0][nums[0]]=1;
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int t=0;t<=s2;t++){
+                int not_take=dp[i-1][t];
+                int take=0;
+                if(nums[i]<=t)take=dp[i-1][t-nums[i]];
+                dp[i][t]=take+not_take;
+            }
+        }
+        return dp[n-1][s2];
     }
 };
