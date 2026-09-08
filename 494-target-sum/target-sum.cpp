0a7562fdeffebf0b88cc28c5218617076be0a4ba
Akshat Gupta <akshat9105@gmail.com>
2026-09-09 00:58:58 +0530

    Time: 7 ms (67.56%) | Memory: 15.5 MB (46.13%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    //Memoization
-    int f(int index,vector<int>&nums,int target,vector<vector<int>>&dp,int &offset){
-        
+    int f(int index,int sum,vector<int>&arr,vector<vector<int>>&dp){
         if(index==0){
-            if(target-nums[index]==0&&target+nums[index]==0){
-                return 2;
-            }
-            if(target-nums[index]==0||target+nums[index]==0){
-                return 1;
-            }
+            if(sum==0&&arr[0]==0)return 2;
+            if(sum==0||sum==arr[0])return 1;
             return 0;
         }
-        if(dp[index][target+offset]!=-1)return dp[index][target+offset];
-        int left=f(index-1,nums,target-nums[index],dp,offset);
-        
-        int right=f(index-1,nums,target+nums[index],dp,offset);
-        
-        return dp[index][target+offset]=left+right;
+        if(dp[index][sum]!=-1)return dp[index][sum];
+        int not_take=f(index-1,sum,arr,dp);
+        int take=0;
+        if(arr[index]<=sum)take=f(index-1,sum-arr[index],arr,dp);
+        return dp[index][sum]=take+not_take;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int sum=0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
         }
-        int offset=sum+abs(target);
-        vector<vector<int>>dp(nums.size(),vector<int>(2*offset+abs(target)+1,-1));
-        return f(nums.size()-1,nums,target,dp,offset);
+        if(sum-target<0)return 0;
+        if((sum-target)%2!=0)return 0;
+        int s2=(sum-target)/2;
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(s2+1,-1));
+        return f(n-1,s2,nums,dp);
     }
 };
